6a6c65e5654915f5ad2dfedf89d707d6e2ea834b
akolight <152660375+akolight@users.noreply.github.com>
2023-12-03 13:35:46 -0800

    call and arrive
    set up the animations and button for sending the train and having it return

--- scripts/3d.gd
+++ scripts/3d.gd
@@ -1,5 +1,14 @@
 extends Node3D
 
+func _ready():
+	$"../Button2".set_visible(true)
 
-func _on_button_pressed():
-	$AnimationPlayer.play("new_animation")
+func _on_button_2_pressed():
+	$"../Button2".set_visible(false)
+	$station_leave.play("station_leave")
+
+func _on_station_arrive_animation_finished(station_arrive):
+	$"../Button2".set_visible(true)
+
+func _on_station_leave_animation_finished(station_leave):
+	$station_arrive.play("station_arrive")
---
+++ scripts/main.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func cycle():
+	pass
